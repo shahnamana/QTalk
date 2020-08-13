a36f864c9624d5190be1753849b8d7fbda292aad
Siddharth <mishrasiddharth1507@gmail.com>
2020-08-13 23:22:44 +0530

    chatapp

---
+++ lib/Pages/HomePage.dart
@@ -0,0 +1,181 @@
+import 'dart:async';
+
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:telegramchatapp/Pages/ChattingPage.dart';
+import 'package:telegramchatapp/main.dart';
+import 'package:telegramchatapp/models/user.dart';
+import 'package:telegramchatapp/Pages/AccountSettingsPage.dart';
+import 'package:telegramchatapp/Widgets/ProgressWidget.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  final String currentUserId;
+  HomeScreen({Key key,@required this.currentUserId}) :super(key:key);
+  @override
+  State createState() => HomeScreenState(currentUserId: currentUserId);
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final String currentUserId;
+
+  HomeScreenState({Key key, @required this.currentUserId});
+  TextEditingController searchTextEditingController=TextEditingController();
+  Future<QuerySnapshot> futureSearchResult;
+  homePageHeader()
+  {
+    return AppBar(
+      automaticallyImplyLeading: false,
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.settings,size: 30.0,color: Colors.white,),
+          onPressed: (){
+            Navigator.push(context, MaterialPageRoute(builder:(context)=>Settings()));
+          },
+        )
+      ],
+      backgroundColor: Colors.lightBlueAccent,
+      title: Container(
+        margin: new EdgeInsets.only(bottom: 4.0),
+        child:TextFormField(
+          style: TextStyle(fontSize: 17.0,color: Colors.white),
+          controller:searchTextEditingController ,
+          decoration: InputDecoration(
+            hintStyle: TextStyle(color: Colors.white),
+            hintText: "Search here.....",
+            enabledBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey),
+
+            ),
+            focusedBorder: UnderlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+              filled: true,
+            prefixIcon: Icon(Icons.person_pin, color: Colors.white, size: 30.0,),
+            suffixIcon: IconButton(
+              icon: Icon(Icons.clear,color: Colors.white,),
+              onPressed: emptyTextFormField,
+            )
+          ),
+        onFieldSubmitted: controlSearching,
+        ),
+      ),
+    );
+  }
+
+  controlSearching(String userName){
+    Future<QuerySnapshot> allFoundUsers=Firestore.instance.collection("users")
+        .where("nickname",isGreaterThanOrEqualTo: userName).getDocuments();
+    setState(() {
+      futureSearchResult=allFoundUsers;
+    });
+  }
+  emptyTextFormField(){
+    searchTextEditingController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: homePageHeader(),
+      body: futureSearchResult==null?displayNoSearchResultScreen():displayUserFoundScreen(),
+    );
+//
+  }
+  displayUserFoundScreen(){
+    return FutureBuilder(
+      future: futureSearchResult,
+      // ignore: missing_return
+      builder: (context,dataSnapshot){
+       if(!dataSnapshot.hasData){
+         return circularProgress();
+       }
+       List<UserResult> searchUserResult=[];
+       dataSnapshot.data.documents.forEach((document){
+         User eachUser=User.fromDocument(document);
+         UserResult userResult=UserResult(eachUser);
+         if(currentUserId!=document["id"]){
+           searchUserResult.add(userResult);
+         }
+       });
+       return ListView(children: searchUserResult);
+      },
+    );
+  }
+
+  displayNoSearchResultScreen(){
+    final Orientation orientation=MediaQuery.of(context).orientation;
+    return Container(
+      child: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Icon(Icons.group,color: Colors.lightBlueAccent,size: 100.0,),
+          Text(
+            "Search Users",
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.lightBlueAccent,fontSize: 50.0,fontWeight: FontWeight.w500),
+          )
+        ],
+      ),
+    );
+  }
+
+
+
+
+}
+
+
+
+
+class UserResult extends StatelessWidget
+{
+  final User eachUser;
+  UserResult(this.eachUser);
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(4.0),
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+
+            GestureDetector(
+              onTap: ()=>sendUserToChatPage(context),
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundColor: Colors.black ,backgroundImage: CachedNetworkImageProvider(eachUser.photoUrl),
+                ),
+                title: Text(
+                  eachUser.nickname,
+                  style: TextStyle(
+                    color: Colors.black,fontSize: 16.0,fontWeight: FontWeight.bold,
+                  ),
+
+                ),
+                subtitle: Text(
+                  "Joined: "+DateFormat("dd MMMM, yyyy - hh:mm:ss")
+                      .format(DateTime.fromMillisecondsSinceEpoch(int.parse(eachUser.createdAt))),
+                  style: TextStyle(color: Colors.grey,fontSize: 14.0,fontStyle: FontStyle.italic),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  sendUserToChatPage(BuildContext context){
+    Navigator.push(context, MaterialPageRoute(builder: (context)=>Chat(receiverId:eachUser.id,receiverAvatar:eachUser.photoUrl,receiverName:eachUser.nickname)));
+  }
+
+}
