fb9a5e17c13c26ef315feaf30388d8cdcae4c81b
Jugal Deepak Chauhan <jugaldc2000@gmail.com>
2020-08-15 20:16:55 +0530

    minor changes

--- Python/app.py
+++ Python/app.py
@@ -67,5 +67,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5050))
+    port = int(os.environ.get('PORT', 5000))
     app.run(port=port, debug=True, use_reloader=False)
