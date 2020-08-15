c1f9c9424d237c16e5320fc6b4918c82b6e78722
shahnamana <naman.as@somaiya.edu>
2020-08-15 17:25:38 +0530

    Updated app.py file

--- Python/app.py
+++ Python/app.py
@@ -1,45 +1,13 @@
+
+
 # Importing the Libraries
 import numpy as np
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import os
-# from sklearn.externals import joblib
-import pickle
 import flask
-import os
-# import newspaper
-# from newspaper import Article
-import urllib
-from newsapi import NewsApiClient
-from difflib import SequenceMatcher
-from statistics import mean
-
-
-news_headlines = []
-news_description = []
-
-
-def new_call(fact_check):
-    newsapi = NewsApiClient(api_key='02e9d787a01340af8eba1c661fffc8ce')
-    global news_headlines
-    global news_description
-
-    top_headlines = newsapi.get_everything(
-        q=fact_check,
-        language='en',
-    )
-    for article in top_headlines['articles']:
-        news_description.append(article['description'])
-        news_headlines.append(article['title'])
-
-
-def similar(a):
-    global news_description
-    prob_list = []
-    for i in range(len(news_description)):
-        prob_list.append(SequenceMatcher(None, a, news_description[i]).ratio())
-    max_in_list = min(prob_list)
-    return max_in_list
+import requests
+from Levenshtein import *
 
 
 # Loading Flask and assigning the model variable
@@ -47,8 +15,38 @@ app = Flask(__name__)
 CORS(app)
 app = flask.Flask(__name__, template_folder='templates')
 
-# with open('model.pickle', 'rb') as handle:
-#     model = pickle.load(handle)
+
+
+def news_call_classifier(fact_check):
+
+    url = "https://bing-news-search1.p.rapidapi.com/news/search"
+    # q = "Amit shah tests negative for COVID-19"
+    querystring = {"count":"50","setLang":"EN","freshness":"Day","textFormat":"Raw","safeSearch":"Off","q": fact_check}
+
+
+    headers = {
+        'x-rapidapi-host': "bing-news-search1.p.rapidapi.com",
+        'x-rapidapi-key': "1daa73315amsha25dced63e1e976p16fe0djsn4b6547ffa9e5",
+        'x-bingapis-sdk': "true"
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    response = response.json()
+
+    news_headline = []
+    for i in range(len(response['value'])):
+        # print(response['value'][i]['name'])
+        # print('\n\n')
+        news_headline.append(response['value'][i]['name'])
+
+    prob_list = []
+    for news_art in news_headline:
+        prob_list.append(ratio(fact_check, news_art))
+        # prob_list.append(jellyfish.jaro_distance(q,news_art))
+    ratio1 = sum(prob_list)/len(prob_list)
+    return ratio1
+
+
 
 
 @app.route('/')
@@ -61,13 +59,11 @@ def main():
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     url = request.get_data(as_text=True)[5:]
-    new_call(url)
-    news = news_description
+    url = url.split(sep="+")
+    url = " ".join(url)
     # Passing the news article to the model and returing whether it is Fake or Real
-    # pred = model.predict(news)
-    maxinlist = similar(url)
-    print(type(news))
-    return render_template('main.html', prediction_text='The news is "{}"'.format(maxinlist))
+    pred = news_call_classifier(url)
+    return render_template('main.html', prediction_text='The news is "{}"'.format(pred))
 
 
 if __name__ == "__main__":
